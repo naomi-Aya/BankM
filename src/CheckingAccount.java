124176879b515a85166fd1cb71e64da5c5d6f1d4
naomi-Aya <naomiayay@gmail.com>
2026-05-26 12:24:17 -1200

    Update CheckingAccount.java

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+// Compte courant
+class CheckingAccount extends BankAccount {
+    private double limit;
 
-public class CheckingAccount {
+    public CheckingAccount(String account, double balance, double limit) {
+        super(account, balance);
+        this.limit = limit;
+    }
+}
+
+// Compte épargne
+class SavingsAccount extends BankAccount {
+    private double interestRate; // Exemple d'attribut pour épargne
+
+    public SavingsAccount(String account, double balance, double interestRate) {
+        super(account, balance);
+        this.interestRate = interestRate;
+    }
+}
+
+// Certificat de dépôt
+class COD extends BankAccount {
+    private int duration; // Durée du certificat
+
+    public COD(String account, double balance, int duration) {
+        super(account, balance);
+        this.duration = duration;
+    }
 }
