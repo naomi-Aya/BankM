1cb6476f0c4574711b4b4e13b7357dcbb6c43469
naomi-Aya <naomiayay@gmail.com>
2026-05-26 12:21:42 -1200

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+// Compte courant
+class CheckingAccount extends BankAccount {
+    private double limit;
 
-public class BankAccount {
+    public CheckingAccount(String account, double balance, double limit) {
+        super(account, balance);
+        this.limit = limit;
+    }
 }
+
+// Compte épargne
+class SavingsAccount extends BankAccount {
+    private double interestRate; // Exemple d'attribut pour épargne
+
+    public SavingsAccount(String account, double balance, double interestRate) {
+        super(account, balance);
+        this.interestRate = interestRate;
+    }
+}
+
+// Certificat de dépôt
+class COD extends BankAccount {
+    private int duration; // Durée du certificat
+
+    public COD(String account, double balance, int duration) {
+        super(account, balance);
+        this.duration = duration;
+    }
+}
+
